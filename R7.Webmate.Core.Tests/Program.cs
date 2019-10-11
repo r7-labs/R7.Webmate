513343ffb8f924475d69189dfac194d1f7605644
roman-yagodin <roman.yagodin@gmail.com>
2019-10-11 12:26:28 +0400

    Add tests project

---
+++ R7.Webmate.Core.Tests/Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace R7.Webmate.Core.Tests
+{
+    class Program
+    {
+        static void Main (string [] args)
+        {
+            Console.WriteLine ("Hello World!");
+        }
+    }
+}
---
+++ R7.Webmate.Core.Tests/Text/Processings/TextProcessingSerializerTests.cs
@@ -0,0 +1,20 @@
+﻿using Xunit;
+using R7.Webmate.Core.Text.Processings;
+
+namespace R7.Webmate.Core.Tests.Text.Processings
+{
+    public class TextProcessingSerializerTests
+    {
+        [Fact]
+        public void RoundtripTest ()
+        {
+            var exception = Record.Exception (() => {
+                var tp = TextProcessingFactory.CreateTextToTextProcessing ();
+                var tpSerializer = new TextProcessingSerializer ();
+                var yaml = tpSerializer.Serialize (tp);
+                var tp2 = tpSerializer.Deserialize (yaml);
+            });
+            Assert.Null (exception);
+        }
+    }
+}

4d4e35dd23be133bfd3b127963dc1c974c7a66aa
roman-yagodin <roman.yagodin@gmail.com>
2020-02-10 12:45:57 +0400

    Bump Xwt

--- R7.Webmate.Xwt/CopyBackendAssemblies.ps1
+++ R7.Webmate.Xwt/CopyBackendAssemblies.ps1
@@ -7,7 +7,7 @@ param (
 
 $PackagesPath = "../packages"
 
-Copy-Item -Path "$PackagesPath/Xwt.Gtk3.$XwtPackageVersion/lib/net40/Xwt.Gtk3.dll" -Destination $OutputPath -PassThru
+Copy-Item -Path "$PackagesPath/Xwt.Gtk3.$XwtPackageVersion/lib/net472/Xwt.Gtk3.dll" -Destination $OutputPath -PassThru
 Copy-Item -Path "$PackagesPath/Xwt.Gtk3.$XwtPackageVersion/build/Xwt.Gtk3.dll.config" -Destination $OutputPath -PassThru
 
 Copy-Item -Path "$PackagesPath/Xwt.WPF.$XwtPackageVersion/lib/net472/Xwt.WPF.dll" -Destination $OutputPath -PassThru

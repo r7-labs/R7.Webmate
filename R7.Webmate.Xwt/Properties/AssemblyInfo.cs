193c929323375ba928844d121024f8c3103eb56b
roman-yagodin <roman.yagodin@gmail.com>
2020-07-20 00:41:49 +0400

    Introduce AssemblyInfo and shared SolutionInfo

---
+++ R7.Webmate.Xwt/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle ("R7.Webmate.Xwt")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCulture ("")]
---
+++ R7.Webmate/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle ("R7.Webmate")]
+[assembly: AssemblyDescription ("")]
+[assembly: AssemblyConfiguration ("")]
+[assembly: AssemblyCulture ("")]

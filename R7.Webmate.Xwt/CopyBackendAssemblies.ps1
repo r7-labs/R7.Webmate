a74807c602eedb178ccc70d18bdc70ad6f8fba11
Roman Yagodin <Roman.Yagodin@lukoilrus.onmicrosoft.com>
2021-09-01 16:46:22 +0300

    Bump Xwt, test switch to Gtk2 on Windows WPF backend isn't working - could be local machine issue?

--- R7.Webmate.Xwt/CopyBackendAssemblies.ps1
+++ R7.Webmate.Xwt/CopyBackendAssemblies.ps1
@@ -9,5 +9,6 @@ $PackagesPath = "../packages"
 
 Copy-Item -Path "$PackagesPath/Xwt.Gtk3.$XwtPackageVersion/lib/net472/Xwt.Gtk3.dll" -Destination $OutputPath -PassThru
 Copy-Item -Path "$PackagesPath/Xwt.Gtk3.$XwtPackageVersion/build/Xwt.Gtk3.dll.config" -Destination $OutputPath -PassThru
-
+Copy-Item -Path "$PackagesPath/Xwt.Gtk.$XwtPackageVersion/lib/net472/Xwt.Gtk.dll" -Destination $OutputPath -PassThru
+Copy-Item -Path "$PackagesPath/Xwt.Gtk.Windows.$XwtPackageVersion/lib/net472/Xwt.Gtk.Windows.dll" -Destination $OutputPath -PassThru
 Copy-Item -Path "$PackagesPath/Xwt.WPF.$XwtPackageVersion/lib/net472/Xwt.WPF.dll" -Destination $OutputPath -PassThru
